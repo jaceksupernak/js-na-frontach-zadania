8b5b52c94a49ce8654375b01e2932fa6389457a7
Jacek Supernak <jj.supernak@gmail.com>
2024-02-14 16:54:36 +0000

    change folder names

--- week-1-ts-ecosystem/w01e01-typescript-basics/src/main.ts
+++ week-1-ts-ecosystem/w01e01-typescript-basics/src/main.ts
@@ -1,5 +1,5 @@
-import { hasAddress, isAdult } from './user.validators';
-import { user } from './user.data';
+import { hasAddress, isAdult } from './validators/user.validators';
+import { user } from './data/user.data';
 
 console.log(`User ${user.name} is ${isAdult(user) ? 'adult' : 'minor'}`);
 console.log(`and has${hasAddress(user) ? '' : ' no'} address`);
